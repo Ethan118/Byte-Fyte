718f17d32ab9cdc691e74e010687e831130ca2fc
GamTam <59884023+GamTam@users.noreply.github.com>
2021-01-22 18:13:08 -0500

    Are you ready for the pain?

---
+++ desktop/src/ca/error404/bytefyte/desktop/DesktopLauncher.java
@@ -0,0 +1,21 @@
+package ca.error404.bytefyte.desktop;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.error404.bytefyte.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.forceExit = false;
+		config.width = 1280;
+		config.height = 720;
+		config.title = "Byte Fyte";
+		config.resizable = false;
+		config.addIcon("icons/mac.png", Files.FileType.Internal);
+		config.addIcon("icons/windows + linux.png", Files.FileType.Internal);
+		config.addIcon("icons/windows old.png", Files.FileType.Internal);
+		new LwjglApplication(new Main(), config);
+	}
+}
