b20c2458fb0fe25a250316450000d51cfba52d77
GamTam <59884023+GamTam@users.noreply.github.com>
2021-02-10 17:26:11 -0500

    more songs!

--- core/src/ca/error404/bytefyte/scene/TestScene.java
+++ core/src/ca/error404/bytefyte/scene/TestScene.java
@@ -66,7 +66,7 @@ public class TestScene implements Screen {
         b2body.createFixture(fdef);
 
         // plays a song so I can hear things
-        game.songFromSeries("other");
+        game.songFromSeries();
         music = game.manager.get("audio/music/" + Main.songName + ".wav", Music.class);
         music.setLooping(true);
         music.play();
