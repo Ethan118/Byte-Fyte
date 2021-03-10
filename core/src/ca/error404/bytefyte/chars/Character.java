b3063c08a6fb8e5d2a4537dfdd1e9907bc5d3aa7
Ethan118 <ethan@tower10labs.com>
2021-03-10 06:55:01 -0500

    added some physics to character

--- core/src/ca/error404/bytefyte/chars/Character.java
+++ core/src/ca/error404/bytefyte/chars/Character.java
@@ -1,12 +1,20 @@
 package ca.error404.bytefyte.chars;
 
+import ca.error404.bytefyte.Main;
+import ca.error404.bytefyte.constants.ControllerButtons;
+import ca.error404.bytefyte.constants.Keys;
+import ca.error404.bytefyte.constants.Tags;
 import ca.error404.bytefyte.scene.TestScene;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.mappings.Xbox;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
+
+import java.util.EventListener;
 
 public abstract class Character extends Sprite {
     private enum State {
@@ -37,7 +45,8 @@ public abstract class Character extends Sprite {
     private int gravity;
     private int friction;
 
-    private float scale;
+    private float spriteScale;
+    private float collisionScale;
 
     private boolean grounded;
 
@@ -47,11 +56,68 @@ public abstract class Character extends Sprite {
     private Animation<TextureRegion> jump;
     private Animation<TextureRegion> fall;
 
-    public Character(TestScene scene, Vector2 spawn) {
+    private Controller controller;
+    private float deadZone = 0.1f;
 
+    private void defBody() {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(pos);
+
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+
+        shape.setAsBox((float) getRegionWidth() / (collisionScale * 2 * Main.PPM), (float) getRegionHeight() / (collisionScale * 2 * Main.PPM));
+
+        fixtureDef.shape = shape;
+        fixtureDef.filter.categoryBits = Tags.PLAYER_BIT;
+        fixtureDef.friction = 0;
+        b2Body.createFixture(fixtureDef).setUserData(this);
+
+        EdgeShape feet = new EdgeShape();
+        feet.set(new Vector2((float) -getRegionWidth() / collisionScale / 2.2f / Main.PPM, -getRegionHeight() / (collisionScale * 1.9f) / Main.PPM), new Vector2((float) getRegionWidth() / collisionScale / 2.2f / Main.PPM, -getRegionHeight() / (collisionScale * 1.9f) / Main.PPM));
+        fixtureDef.isSensor = true;
+        fixtureDef.shape = feet;
+        fixtureDef.filter.categoryBits = Tags.PLAYER_FEET_BIT;
+        b2Body.createFixture(fixtureDef).setUserData(this);
+
+        EdgeShape head = new EdgeShape();
+        head.set(new Vector2((float) -getRegionWidth() / collisionScale / 2.2f / Main.PPM, getRegionHeight() / (collisionScale * 1.9f) / Main.PPM), new Vector2((float) getRegionWidth() / collisionScale / 2.2f / Main.PPM, getRegionHeight() / (collisionScale * 1.9f) / Main.PPM));
+        fixtureDef.isSensor = true;
+        fixtureDef.shape = head;
+        fixtureDef.filter.categoryBits = Tags.PLAYER_HEAD_BIT;
+        b2Body.createFixture(fixtureDef).setUserData(this);
+    }
+
+    public Character(TestScene scene, Vector2 spawn, Controller controller) {
+        this.world = scene.getWorld();
+        this.controller = controller;
+
+        this.pos = spawn;
+
+        state = State.IDLE;
+        prevState = State.IDLE;
+
+        defBody();
+
+        setBounds(b2Body.getPosition().x - getWidth() / 2, (b2Body.getPosition().y - getHeight() / 2), (float) getRegionWidth() / spriteScale / Main.PPM, (float) getRegionHeight() / spriteScale / Main.PPM);
+    }
+
+    private void handleInput() {
+        Vector2 moveVector = new Vector2();
+
+        if (Main.controllers.size > 0) {
+            moveVector.x = Math.abs(controller.getAxis(ControllerButtons.L_STICK_HORIZONTAL_AXIS)) >= deadZone ? Math.abs(controller.getAxis(ControllerButtons.L_STICK_HORIZONTAL_AXIS)) : 0f;
+        } else {
+            if (Gdx.input.isKeyPressed(Keys.MOVE_RIGHT)) moveVector.x += 1;
+            if (Gdx.input.isKeyPressed(Keys.MOVE_LEFT)) moveVector.x -= 1;
+        }
     }
 
     private void update() {
+        handleInput();
+
 
     }
 
