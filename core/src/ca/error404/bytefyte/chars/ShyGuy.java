1add1cee1e56152fe106265c3ab5cd3b701a86ef
Michael <mdelduca04@gmail.com>
2021-03-11 18:16:00 -0500

    Added attack keys. Added directional inputs. Added attack inputs based on direction.

--- core/src/ca/error404/bytefyte/chars/ShyGuy.java
+++ core/src/ca/error404/bytefyte/chars/ShyGuy.java
@@ -16,96 +16,111 @@ public class ShyGuy extends Character{
 
     @Override
     void basicNeutral() {
-
+        System.out.println("Basic Neutral");
     }
 
     @Override
     void basicSide() {
-
+        System.out.println("Basic Side");
     }
 
     @Override
     void basicUp() {
-
+        System.out.println("Basic Up");
     }
 
     @Override
     void basicDown() {
-
+        System.out.println("Basic Down");
     }
 
     @Override
     void dashAttack() {
+        System.out.println("Dash Attack");
 
     }
 
     @Override
     void smashNeutral() {
+        System.out.println("Smash Neutral");
 
     }
 
     @Override
     void smashSide() {
+        System.out.println("Smash Side");
 
     }
 
     @Override
     void smashUp() {
+        System.out.println("Smash Up");
 
     }
 
     @Override
     void smashDown() {
+        System.out.println("Smash Down");
 
     }
 
     @Override
     void specialNeutral() {
+        System.out.println("Special Neutral");
 
     }
 
     @Override
     void specialSide() {
+        System.out.println("Special Side");
 
     }
 
     @Override
     void specialUp() {
+        System.out.println("Special Up");
 
     }
 
     @Override
     void specialDown() {
+        System.out.println("Special Down");
 
     }
 
     @Override
     void ultimate() {
+        System.out.println("Ultimate");
 
     }
 
     @Override
     void airNeutral() {
+        System.out.println("Air Neutral");
 
     }
 
     @Override
     void airLeft() {
+        System.out.println("Air Left");
 
     }
 
     @Override
     void airRight() {
+        System.out.println("Air Right");
 
     }
 
     @Override
     void airUp() {
+        System.out.println("Air Up");
 
     }
 
     @Override
     void airDown() {
+        System.out.println("Air Down");
 
     }
 }
