edbca01154651f2005d3171ac8802d39b53c9b09
gram <grpahi@gmail.com>
2021-04-25 20:36:27 -0400

    added maps B castle and Dimble wood

--- core/assets/sprites/single player/bowser's castle/Bowser's Castle.tsx
+++ core/assets/sprites/single player/bowser's castle/Bowser's Castle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Bowser's Castle" tilewidth="16" tileheight="16" tilecount="30" columns="10">
+<tileset version="1.5" tiledversion="1.6.0" name="Bowser's Castle" tilewidth="16" tileheight="16" tilecount="30" columns="10">
  <image source="Bowser's Castle.png" width="160" height="48"/>
 </tileset>
---
+++ core/assets/sprites/single player/bowser's castle/bowser's castle_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="bowser's castle_background" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="bowser's castle_background.png" width="512" height="512"/>
+</tileset>
