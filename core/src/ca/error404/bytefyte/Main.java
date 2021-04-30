f10653d8cb71f7fd8c19380445b94771f2637c97
Ethan118 <ethan@tower10labs.com>
2021-04-30 00:53:48 -0400

    goodbye pile

--- core/src/ca/error404/bytefyte/Main.java
+++ core/src/ca/error404/bytefyte/Main.java
@@ -140,21 +140,7 @@ public class Main extends Game {
 
 		reloadControllers();
 
-//		The Pile™
-
-//		CharacterSelect.characters = new String[] {"madeline", "masterchief", "shyguy", "kirby"};
-		CharacterSelect.characters = new String[] {"marioluigi", "marioluigi", null, null};
-//		CharacterSelect.characters = new String[] {"shyguy", "masterchief", null, null};
-//		CharacterSelect.characters = new String[] {"madeline", "masterchief", "shyguy", "kirby"};
-//		setScreen(new LoadBattleMap("Forsaken City", this, new Vector2(0.5f, 0), "celeste"));
-//		setScreen(new LoadBattleMap("Russia", this, new Vector2(0.5f, 0), "russia"));
-//		setScreen(new LoadBattleMap("Halberd", this, new Vector2(-350, 0), "kirby"));
-		setScreen(new LoadBattleMap("Training Room", this, new Vector2(0, 0), null));
-//		setScreen(new LoadBattleMap("Fawful's Castle", this, new Vector2(0, 0), "mal"));
-//		setScreen(new LoadBattleMap("Castle Bleck", this, new Vector2(20, 0), "paper mario"));
-//		setScreen(new LoadBattleMap("Flowchart", this, new Vector2(50, -3000), null));
-//		setScreen(new LoadBossRoom("dimble wood/Dimble Wood Boss", "dimble wood/Dimble Wood Tileset", "dimble wood/Dimble Wood Boss_background", this, new Vector2(0, 0), "boss theme"));
-//		setScreen(new TitleScreen(this));
+		setScreen(new TitleScreen(this));
 	}
 
 	public void reloadControllers() {
